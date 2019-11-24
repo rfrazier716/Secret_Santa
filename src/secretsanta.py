52eefb7259fd391b78448b9a8f70839af1ebe1e6
Frazier, Ryan <Ryan.Frazier@analog.com>
2019-11-24 18:26:22 -0500

    updated program flow. optimized stack based match engine now used and functioning

--- src/secretsanta.py
+++ src/secretsanta.py
@@ -16,9 +16,11 @@ class Player(object):
 		self.target=""
 
 class MatchEngine():
+	ENCODED_PLAYER_WIDTH=7	#how many bits are designated for player indices
+	STACK_DIVIDER=0x1<<(2*ENCODED_PLAYER_WIDTH+1)	#Value written to stack to tell program to stop pulling
+
 	def __init__(self,mutual_exclusion:True):
 		self.mutual_exclusion=mutual_exclusion
-		self.stack=[]	#Stack that's used for spanning the choice matrix
 		self.players=[] #List of the players
 
 	def n_players(self):
@@ -26,6 +28,7 @@ class MatchEngine():
 		return len(self.players)
 
 	def load_players(self,player_list_file):
+		self.players=[1,2,3,4,5]
 		self.build_target_matrix()
 		pass
 		#TODO: make it load the players from a .json file
@@ -34,112 +37,96 @@ class MatchEngine():
 	def build_target_matrix(self):
 		#
 		self.target_matrix=np.array([
-				[0,1,0,0,0],
-				[1,1,1,0,0],
+				[0,1,0,1,0],
+				[1,0,1,0,0],
 				[0,0,0,1,1],
-				[1,1,0,0,0],
+				[1,1,0,0,1],
 				[1,0,1,1,0]
 				])
 
-	def push_index_to_stack(self,row,column):
-		# pushes a matrix coordinate onto the stack
-		self.stack.append(row)
-		self.stack.append(column)
-		pass
+	def encode_index(self,index):
+		#accepts a tuple that is the row column value
+		return (index[0]<<(self.ENCODED_PLAYER_WIDTH+1))+index[1]
 
-	def pull_index_from_stack(self):
-		#pulls a matrix coordinate off the stack and returns it as row, column
-		column=self.stack.pop(-1)
-		row=self.stack.pop(-1)
+	def decode_index(self,encoded_index):
+		#accepts a 16 bit number and returns a tuple of (row,column)
+		row=(encoded_index)>>(self.ENCODED_PLAYER_WIDTH+1)
+		column=(encoded_index & ((1<<(self.ENCODED_PLAYER_WIDTH+1))-1))
 		return row,column
 
-	def null_potential_match(self,player,match):
-		#writes a zero to the target matrix to remove the chancce that a player 
-		pass
+	def assign_match(self,player,target):
+		self.target_matrix[player,target]=0	#Set match to zero in target matrix
+		
+		#Write matched player to stack
+		stack_value=self.encode_index((player,target))	
+		self.stack.append(stack_value)	
+		self.stack.append(self.STACK_DIVIDER)	#Write stack divider value
 
-	def assign_match(self,player):
-		match=np.random.choice(np.nonzero(self.target_matrix[player,:])[0]) #Choose a match randomly based on the possible pairings
-		#write that stack with a zero so that if it gets pulled the option in the tree is deleted
+		#Update target matrix so no other players can be assigned the same target
+		for j in range(player+1,self.n_players()): 
+			if self.target_matrix[j,target]==1: #check if index is even a potential match
+				
+				#encode and write to stack
+				stack_value=self.encode_index((j,target))
+				self.stack.append(stack_value)
 
-		return match #return the match
-
-
-
-
+				self.target_matrix[j,target]=0	#set to zero in target matrix
 
 	def undo_last_match(self):
-		indices_to_revert=stack.pop(-1) #The most recent stack value should say how many indices were written
-		for _ in range(indices_to_revert):
-			col=stack.pop(-1)	#Pull the row off the stack
-			row=stack.pop(-1)	#Pull the column off the stack
-			self.target_matrix[row,col]=1 #Revert the XYCoord to it's last state
+		pass
+		finished=False
+		while not finished:
+			stack_value=self.stack.pop(-1)
+			if stack_value==self.STACK_DIVIDER:	
+				#If we pull off a divider, stop pulling values
+				finished=True
+			else:
+				#convert the value to a row column pair and write a 1 to that value in the target matrix
+				row,column=self.decode_index(stack_value)
+				self.target_matrix[row,column]=1
 
+
+	def print_stack(self):
+		for val in self.stack:
+			print("{:04X}".format(val))
 
 	def match_all_players(self):
-		#Return a tuple of  [[player_0,target_0],...,[player_n,target_n]] form
+		#returns a list of all matches such that player[n] is matched to player[list[n]]
+		
 		#Sort target matrix from least to greatest number of potential matches
-		player_sort_indices=np.argsort(np.sum(space,1)) # The player indices sorted by number of matches
+		player_sort_indices=np.argsort(np.sum(self.target_matrix,1)) # The player indices sorted by number of matches
 		self.target_matrix=self.target_matrix[player_sort_indices,:] #Rearrange the target matrix from fewest to largest matches
 		player_to_match=0 #which player is currently being assigned a target
-		match_list=[]
+		match_list=[]	#List that keeps track of who matched with who, needs to be reordered after all matches are assigned
+		self.stack=[]	#empty the stack
 		
 		while player_to_match<self.n_players():
 			#Check if a valid solution space still exists
-			if np.in1d(np.sum(sorted_space,1),0).any():	
+			if np.in1d(np.sum(self.target_matrix[player_to_match:],1),0).any():	#if the sum of options for any remaining players is zero
 				#Solution space is no longer valid, revert to a previous choice
+				match_list.pop(-1)
 				self.undo_last_match()
+				player_to_match-=1
 			else:
-				self.assign_match()
-			#If it does exist assign a new match and push all changes to the queue
+				target=np.random.choice(np.nonzero(self.target_matrix[player_to_match,:])[0]) #Randomly pick a target for the current player based on options in target matrix
+				self.assign_match(player_to_match,target) #Assign a new match
+				match_list.append(target)
+				player_to_match+=1
 
+			#print(self.target_matrix)
+			#print("player:{}\tCurrent Stack".format(player_to_match))
+			#self.print_stack()
+			#print("")
 
+		sorted_matches=[match_list[index] for index in player_sort_indices] #change the match list back to its original order
+		return sorted_matches #return the list of matches
 
-def generate_matches(state_space):
-	#State Space is a 2D binary array where state_space[n,m] determines if player m can be assigned player n
-	#Returns a 1D list of every players partner
-
-	#Algorithm uses a stack with following chunks written at any point
-
-	#Sort state space based on how many potential matches there are in ascending order
-	space=np.array(state_space) #state space as a numpy array
-	player_sort=np.argsort(np.sum(space,1)) # The player indices sorted by number of matches
-	sorted_space=space[player_sort,:]
-	
-	stack=[] #Stack to holds states 
-	tree_depth=0 #How far down the tree gone
-	max_tree_depth=sorted_space.shape[0]
-
-	while tree_depth<max_tree_depth:
-		if np.in1d(np.sum(sorted_space,1),0).any():	#if a zero exists for matches for a player (aka this branch does not have a valid solution)
-			if g_debug:
-				print("Current Path has no solution, reverting")
-
-			try:
-				failed_branch=stack.pop(-1)	# Try to Pop the node off the stack
-				sorted_space[tree_depth:,failed_branch]=1
-			except IndexError as e: # the stack is empty, meaning there's no valid solution
-				raise ValueError("Provided state space has no solution, check your parameters")
-
-			sorted_space[tree_depth,decision_history[-1]]=0	#Delete this branch from space because it's a bad solution
-			decision_history.pop(-1) #remove the choice from history
-		else:
-			#pick one to match to and put unmodified space and decision on stack
-			branch=np.random.choice(np.nonzero(sorted_space[tree_depth,:])[0])
-			stack.append(branch) # Append the choice to the stack			
-			sorted_space[tree_depth+1:,branch]=0 #Modify space based on choice
-			tree_depth+=1	#increment tree depth
-
-		if g_debug:
-			print("tree Depth:\t{}".format(tree_depth))
-			print("Match History:\t{}".format(decision_history))
-			print("Current State Space:\t")
-			print(sorted_space)
-	return decision_history[player_sort]
+def import_player_list(file):
+	pass
 
 def main():
-	engine=MatchEngine(False)
-	engine.load_players(1)
-	print(engine.target_matrix)
+	player_list_file=Path(sys.argv[0]) / 'private' / 'player_list.json'
+	players=import_player_list(player_list_file)
 
 if __name__=='__main__':
 	main()
